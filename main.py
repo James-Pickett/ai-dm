b2bed06afd371681059a24e795f5976242a02015
jaypickle <you@example.com>
2025-02-12 20:11:08 -0800

    good checkpoint, system prompt set up

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import config
 import models
 import my_logging
 
+def init_dungeon_master(config):
+    model_name = config.get("dungeon_master_model")
+    system_prompt = config.get("dungeon_master_system_prompt")
+
+    if not model_name:
+        raise ValueError("model name cannot be empty")
+
+    if not system_prompt:
+        raise ValueError("system prompt cannot be empty")
+
+    return models.DungeonMaster(model_name, "Dungeon Master", system_prompt)
+
 if __name__ == '__main__':
     my_logging.setup()
 
     config = config.load('./config.yml')
-
-    dungeon_master = models.DungeonMaster(config.get("dungeon_master_model"))
+    dungeon_master = init_dungeon_master(config)
 
     while True:
         user_input = input("You: ").strip()
--- models.py
+++ models.py
@@ -2,12 +2,19 @@ import mlx_lm
 import logging
 
 class _Model:
-    def __init__(self, model_name):
-        if not model_name:
+    def __init__(self, model_path, custom_model_name):
+        if not model_path:
             raise ValueError("model name cannot be empty")
-        self.model, self.tokenizer = mlx_lm.load(model_name)
+        self.llm_name = model_path
+        self.custom_model_name = custom_model_name
+        self.model, self.tokenizer = mlx_lm.load(model_path)
 
-    def chat(self, messages):
+    def chat(self, messages, system_prompt=None):
+        if system_prompt is not None:
+            messages.insert(0, {"role": "system", "content": system_prompt})
+
+        logging.debug(f"sending to {self.custom_model_name} {self.llm_name} {messages}")
+
         if self.tokenizer.chat_template is not None:
             prompt = self.tokenizer.apply_chat_template(
                 messages, add_generation_prompt=True
@@ -16,13 +23,14 @@ class _Model:
         return mlx_lm.generate(self.model, self.tokenizer, prompt=prompt, max_tokens=-1)
 
 class DungeonMaster(_Model):
-    def __init__(self, model_name):
-        super().__init__(model_name)
+    def __init__(self, model_path, custom_model_name, system_prompt):
+        super().__init__(model_path, custom_model_name)
         self.chat_history = []
+        self.system_prompt = system_prompt
 
     def chat(self, prompt):
         self.chat_history.append({"role": "user", "content": prompt})
-        response = super().chat(self.chat_history)
+        response = super().chat(self.chat_history.copy(), self.system_prompt)
         self.chat_history.append({"role": "assistant", "content": response})
         return response
 
